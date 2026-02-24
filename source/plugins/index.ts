384c0b51270d4de2aededf719b9d1dea83be7001
Jason Hyde <github@2bad.me>
2026-02-24 02:50:54 +0100

    [ts] remove eslint config generator and all plugin modules

--- source/plugins/index.ts
+++
@@ -1,7 +0,0 @@
-// biome-ignore lint/performance/noBarrelFile: acceptable for this case
-export { config as import } from './import.ts'
-export { config as jsdoc } from './jsdoc.ts'
-export { config as neostandard } from './neostandard.ts'
-export { config as node } from './node.ts'
-export { config as promise } from './promise.ts'
-export { config as vitest } from './vitest.ts'
