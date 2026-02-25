899a98ebad2bf63c2e60a5f9ee61f533f28f70d6
Jason Hyde <github@2bad.me>
2026-02-25 02:32:06 +0100

    [conf] add coverage thresholds to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,19 @@
-/* eslint-disable import-x/no-default-export */
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { coverageConfigDefaults, defineConfig } from 'vitest/config'
 
-// biome-ignore lint/style/noDefaultExport:
 export default defineConfig({
   test: {
     exclude: ['build', 'node_modules'],
     coverage: {
       exclude: ['build', ...coverageConfigDefaults.exclude],
-      provider: 'v8'
+      include: ['source/**/*.{ts,tsx}'],
+      provider: 'v8',
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80
+      }
     },
     testTimeout: 30000
   },
